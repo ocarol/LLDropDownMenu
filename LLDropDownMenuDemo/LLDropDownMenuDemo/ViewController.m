a8884cd6150b79cda0adbe45e5c3dec85ee41d9f
ocarol <yulili2020@qq.com>
2016-01-07 13:22:09 +0800

    上传demo，优化主文件

--- LLDropDownMenuDemo/LLDropDownMenuDemo/ViewController.m
+++ LLDropDownMenuDemo/LLDropDownMenuDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LLDropDownMenu.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"下拉Demo";
+    __weak __typeof(self) weakSelf = self;
+    UIImage *img = [UIImage imageNamed:@"arrow-left-default"];
+    // 将图片旋转90度
+    img = [UIImage imageWithCGImage:img.CGImage scale:1 orientation:UIImageOrientationLeft];
+    NSArray *data = @[@"天安门",@"西直门",@"宣武门",@"八达岭",@"公主坟",@"复兴门",@"水立方",@"三棵树",@"王府井",@"西三旗",@"西二旗",@"回龙观",@"天通苑",@"圆明园",@"国家图书馆",@"中山公园",@"科技园"];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:[[LLDropDownMenu alloc] initWithTitle:@"区域" image:img data:data menuDidSelected:^(NSString *selectedItemTitle) {
+        //添加模糊请求代码
+        weakSelf.title = selectedItemTitle;
+        
+    }]];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
